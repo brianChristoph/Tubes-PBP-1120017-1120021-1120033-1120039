321eb5ef91cba1210161aba092914b661f039051
panjiandika1011 <panjiandika861@gmail>
2022-04-18 01:00:51 +0700

    commit

--- controllers/transaction_controller.go
+++ controllers/transaction_controller.go
@@ -15,11 +15,15 @@ func BookingSeats(c *gin.Context) {
 
 	// get status
 	msId := c.PostForm("ms_id")
+	print(msId)
 	var seatStatus int
 	seatId := c.PostForm("seat_id")
+	print(seatId)
 	studioId := c.PostForm("studio_id")
+	print(studioId)
 	row := db.QueryRow("SELECT status from studio_seat JOIN seats ON studio_seat.seat_id = seats.id WHERE seats.id = ? AND studio_id = ?", seatId, studioId)
 	if err := row.Scan(&seatStatus); err != nil {
+		print("a")
 		panic(err.Error())
 	}
 	if seatStatus == 0 {
@@ -29,16 +33,20 @@ func BookingSeats(c *gin.Context) {
 	// Cek trans id
 	var count int
 	transId := c.PostForm("trans_id")
-	row2 := db.QueryRow("SELECT count(*) from transactions WHERE trans_id = ?", transId)
+	print(transId)
+	row2 := db.QueryRow("SELECT count(*) from transactions WHERE id = ?", transId)
 	if err := row2.Scan(&count); err != nil {
+		print("b")
 		panic(err.Error())
 	}
 
 	// buat trans jika belum ada
+	println(count)
 	if count == 0 {
 		personId := c.PostForm("person_id")
 		_, errQuery := db.Exec("INSERT INTO transactions (person_id) values (?)", personId)
 		if errQuery != nil {
+			print("c")
 			panic(errQuery)
 		}
 	}
@@ -46,13 +54,15 @@ func BookingSeats(c *gin.Context) {
 	// insert detail trans
 	_, errQuery := db.Exec("INSERT INTO detail_transactions (transaction_id, seat_id, ms_id) values (?, ?, ?)", transId, seatId, msId)
 	if errQuery != nil {
+		print("d")
 		panic(errQuery)
 	}
 	// Update status seat
 	_, errQuery2 := db.Exec("UPDATE studio_seat SET status = 0 WHERE seat_id = ?", seatId)
 	if errQuery2 != nil {
+		print("e")
 		panic(errQuery2)
 	}
 	// cara masukin message sukses?
-
+	print("f")
 }
