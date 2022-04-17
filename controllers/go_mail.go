28a7296df501a2d6838fab2d4e331d5ee567b020
Matthew <matthewhariady@gmail.com>
2022-04-17 17:29:16 +0700

    register,gomail

--- controllers/go_mail.go
+++ controllers/go_mail.go
@@ -1,4 +1,4 @@
-package api
+package controllers
 
 import (
 	"fmt"
@@ -13,10 +13,9 @@ const CONFIG_SENDER_NAME = "PT. Bioskop Fox <bioskopfoxofficial@gmail.com>"
 const CONFIG_AUTH_EMAIL = "bioskopfoxofficial@gmail.com"
 const CONFIG_AUTH_PASSWORD = "bioskopfox123"
 
-func sendMail(to []string, cc []string, subject, message string) error {
+func sendMail(to []string, subject, message string) error {
 	body := "From: " + CONFIG_SENDER_NAME + "\n" +
 		"To: " + strings.Join(to, ",") + "\n" +
-		"Cc: " + strings.Join(cc, ",") + "\n" +
 		"Subject: " + subject + "\n\n" +
 		message
 
@@ -27,9 +26,10 @@ func sendMail(to []string, cc []string, subject, message string) error {
 	//CONFIG_AUTH_PASSWORD, adalah password alamat email yang digunakan untuk mengirim email.
 	//smtpAddr = untuk kombinasi host dan port mail server
 
-	err := smtp.SendMail(smtpAddr, auth, CONFIG_AUTH_EMAIL, append(to, cc...), []byte(body))
+	err := smtp.SendMail(smtpAddr, auth, CONFIG_AUTH_EMAIL, to, []byte(body))
 	//sendMail() digunakan untuk mengirim email. Empat data yang disisipkan pada fungsi tersebut dijadikan satu dalam format tertentu, lalu disimpan ke variabel body.
 	if err != nil {
+		fmt.Print("ini error di dalem send Mail")
 		return err
 	}
 
@@ -38,12 +38,12 @@ func sendMail(to []string, cc []string, subject, message string) error {
 
 func GoMail(userEmail string) {
 	to := []string{userEmail}
-	cc := []string{""}
 	subject := "Register Success Message!"
 	message := "Congratulations, your email has successfully registered at Bioskop Fox !!"
 
-	err := sendMail(to, cc, subject, message)
+	err := sendMail(to, subject, message)
 	if err != nil {
+		fmt.Print("ini setelah send Mail")
 		log.Fatal(err.Error())
 	}
 
