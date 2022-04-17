1b403ab17939e466da9aefa7b778db23ebea4648
panjiandika1011 <panjiandika861@gmail>
2022-04-18 00:18:08 +0700

    booking seats

--- controllers/movie_controller.go
+++ controllers/movie_controller.go
@@ -71,7 +71,6 @@ func ShowStreamingList(c *gin.Context) {
 	defer db.Close()
 
 	query := ("SELECT DISTINCT(m.movie_name), m.thumbnail_path FROM streaming_movies sm JOIN movies m ON sm.movie_id = m.id")
-
 	rows, err := db.Query(query)
 	if err != nil {
 		return
--- controllers/transaction_controller.go
+++ controllers/transaction_controller.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	// m "github.com/Tubes-PBP/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,4 +12,48 @@ func TransactionBuyTicket(c *gin.Context) {
 func BookingSeats(c *gin.Context) {
 	db := connect()
 	defer db.Close()
+
+	// get status
+	msId := c.PostForm("ms_id")
+	var seatStatus int
+	seatId := c.PostForm("seat_id")
+	studioId := c.PostForm("studio_id")
+	row := db.QueryRow("SELECT status from studio_seat JOIN seats ON studio_seat.seat_id = seats.id WHERE seats.id = ? AND studio_id = ?", seatId, studioId)
+	if err := row.Scan(&seatStatus); err != nil {
+		panic(err.Error())
+	}
+	if seatStatus == 0 {
+		panic("seat sudah di pesan orang lain")
+		return
+	}
+
+	// Cek trans id
+	var count int
+	transId := c.PostForm("trans_id")
+	row2 := db.QueryRow("SELECT count(*) from transactions WHERE trans_id = ?", transId)
+	if err := row2.Scan(&count); err != nil {
+		panic(err.Error())
+	}
+
+	// buat trans jika belum ada
+	if count == 0 {
+		personId := c.PostForm("person_id")
+		_, errQuery := db.Exec("INSERT INTO transactions (person_id) values (?)", personId)
+		if errQuery != nil {
+			panic(errQuery)
+		}
+	}
+
+	// insert detail trans
+	_, errQuery := db.Exec("INSERT INTO detail_transactions (transaction_id, seat_id, ms_id) values (?, ?, ?)", transId, seatId, msId)
+	if errQuery != nil {
+		panic(errQuery)
+	}
+	// Update status seat
+	_, errQuery2 := db.Exec("UPDATE studio_seat SET status = 0 WHERE seat_id = ?", seatId)
+	if errQuery2 != nil {
+		panic(errQuery2)
+	}
+	// cara masukin message sukses?
+
 }
