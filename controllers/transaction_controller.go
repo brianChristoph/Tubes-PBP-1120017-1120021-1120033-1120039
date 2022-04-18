25fa57f6c1b2a2381a2ca20b6a917105b87a6b2a
panjiandika1011 <panjiandika861@gmail>
2022-04-18 23:52:39 +0700

    change message

--- controllers/transaction_controller.go
+++ controllers/transaction_controller.go
@@ -23,7 +23,7 @@ func BookingSeats(c *gin.Context) {
 	theaterId := c.PostForm("theater_id")
 	row := db.QueryRow("SELECT DISTINCT(studio_seat.status) from studio_seat JOIN seats ON studio_seat.seat_id = seats.id WHERE seats.id = ? AND studio_id = ?", seatId, studioId)
 	if err := row.Scan(&seatStatus); err != nil {
-		panic(err.Error())
+		ErrorMessage(c, http.StatusBadRequest, "Query Error")
 	}
 	if seatStatus == 0 {
 		panic("seat sudah di pesan orang lain")
@@ -34,7 +34,7 @@ func BookingSeats(c *gin.Context) {
 	transId := c.PostForm("trans_id")
 	row2 := db.QueryRow("SELECT count(*) from transactions WHERE id = ?", transId)
 	if err := row2.Scan(&count); err != nil {
-		panic(err.Error())
+		ErrorMessage(c, http.StatusBadRequest, "Query Error")
 	}
 
 	// buat transaksi jika belum ada
@@ -43,31 +43,30 @@ func BookingSeats(c *gin.Context) {
 		personId := c.PostForm("person_id")
 		_, errQuery := db.Exec("INSERT INTO transactions (person_id) values (?)", personId)
 		if errQuery != nil {
-			panic(errQuery)
+			ErrorMessage(c, http.StatusBadRequest, "Query Error")
 		}
 	}
 
 	// insert detail transaksi
 	_, errQuery := db.Exec("INSERT INTO detail_transactions (transaction_id, seat_id, studio_id, theater_id, ms_id) values (?, ?, ?, ?, ?)", transId, seatId, studioId, theaterId, msId)
 	if errQuery != nil {
-		panic(errQuery)
+		ErrorMessage(c, http.StatusBadRequest, "Query Error")
+	} else {
+		SuccessMessage(c, http.StatusCreated, "Berhasil Insert detail Transaksi")
 	}
 	// Update status seat
 	_, errQuery2 := db.Exec("UPDATE studio_seat SET status = 0 WHERE seat_id = ?", seatId)
 	if errQuery2 != nil {
-		panic(errQuery2)
+		ErrorMessage(c, http.StatusBadRequest, "Query Error")
+	} else {
+		SuccessMessage(c, http.StatusCreated, "Berhasil Update status")
 	}
 
 	// Update quantity
 	_, errQuery3 := db.Exec("UPDATE theater_studio SET quantity = quantity-1 WHERE theater_id = ?", theaterId)
 	if errQuery3 != nil {
-		panic(errQuery3)
-	}
-
-	// message sukses
-	if errQuery2 != nil {
 		ErrorMessage(c, http.StatusBadRequest, "Query Error")
 	} else {
-		SuccessMessage(c, http.StatusCreated, "Sukses")
+		SuccessMessage(c, http.StatusCreated, "Berhasil Update Quantity")
 	}
 }
